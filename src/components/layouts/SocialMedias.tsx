f1c9001ba385eba38dd60bad0e7952dfb4769caa
laidanimounir <laidanimounir606@gmail.com>
2026-05-07 03:00:26 +0100

    style: replace social links with Facebook and Instagram for ibro-kitchen

--- src/components/layouts/SocialMedias.tsx
+++ src/components/layouts/SocialMedias.tsx
@@ -11,25 +11,15 @@ type Props = {
 function SocialMedias({ containerClassName, itemsClassName }: Props) {
   return (
     <div className={cn("flex gap-x-5", containerClassName)}>
-      <Link href="https://github.com/clonglam/HIYORI-master" target="_blank">
-        <Icons.gitHub
+      <Link href="https://facebook.com/ibro-kitchen" target="_blank">
+        <Icons.facebook
           className={cn(
             "w-4 h-4 md:w-5 md:h-5 text-muted-foreground hover:text-primary",
             itemsClassName,
           )}
         />
       </Link>
-
-      <Link href="https://twitter.com/ClongLam" target="_blank">
-        <Icons.twitter
-          className={cn(
-            "w-4 h-4 md:w-5 md:h-5 text-muted-foreground hover:text-primary",
-            itemsClassName,
-          )}
-        />
-      </Link>
-
-      <Link href="https://hugo-coding.com" target="_blank">
+      <Link href="https://instagram.com/ibro-kitchen" target="_blank">
         <Icons.globe
           className={cn(
             "w-4 h-4 md:w-5 md:h-5 text-muted-foreground hover:text-primary",
