3be71db73ecbc5e5cddd484ae28ffcf691e38dd5
laidanimounir <laidanimounir606@gmail.com>
2026-05-07 02:40:25 +0100

    refactor: extract NavActions as server component

---
+++ src/components/layouts/NavActions.tsx
@@ -0,0 +1,27 @@
+import { Suspense } from "react";
+import { CartLink, CartNav } from "../../features/carts";
+import { UserNav } from "@/features/auth";
+import { Icons } from "./icons";
+import Link from "next/link";
+
+interface NavActionsProps {
+  adminLayout?: boolean;
+}
+
+async function NavActions({ adminLayout = false }: NavActionsProps) {
+  return (
+    <>
+      <Suspense>
+        <UserNav />
+      </Suspense>
+      <Link href="/wish-list" className="hover:opacity-70 transition-opacity">
+        <Icons.heart className="w-5 h-5" aria-label="wishlist" />
+      </Link>
+      <Suspense fallback={<CartLink productCount={0} />}>
+        {!adminLayout && <CartNav />}
+      </Suspense>
+    </>
+  );
+}
+
+export default NavActions;
