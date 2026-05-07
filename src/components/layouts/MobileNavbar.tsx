85e1fc0bf8b0f3befccf0a0d329a36a6e6b944cc
laidanimounir <laidanimounir606@gmail.com>
2026-05-07 02:12:39 +0100

    style: add missing icons to MobileNavbar (UserNav, wishlist)

--- src/components/layouts/MobileNavbar.tsx
+++ src/components/layouts/MobileNavbar.tsx
@@ -4,21 +4,33 @@ import Branding from "./Branding";
 import MobileSearchInput from "./MobileSearchInput";
 import { SideMenu } from "./SideMenu";
 import CartLink from "../../features/carts/components/CartLink";
+import { UserNav } from "@/features/auth";
+import { Icons } from "./icons";
+import Link from "next/link";
 
 type Props = { adminLayout: boolean };
 
 function MobileNavbar({ adminLayout }: Props) {
   return (
-    <div className="md:hidden flex gap-x-8 justify-between items-center h-[64px]">
-      <div className="flex gap-x-3 items-center">
+    <div className="md:hidden flex justify-between items-center h-[64px]">
+      <div className="flex gap-x-2 items-center">
         <SideMenu />
         <MobileSearchInput />
       </div>
 
       <Branding />
-      <Suspense fallback={<CartLink productCount={0} />}>
-        {!adminLayout && <CartNav />}
-      </Suspense>
+
+      <div className="flex gap-x-4 items-center">
+        <Suspense>
+          <UserNav />
+        </Suspense>
+        <Link href="/wish-list" className="text-zinc-700 hover:text-zinc-900 transition-colors">
+          <Icons.heart className="w-5 h-5" />
+        </Link>
+        <Suspense fallback={<CartLink productCount={0} />}>
+          {!adminLayout && <CartNav />}
+        </Suspense>
+      </div>
     </div>
   );
 }
